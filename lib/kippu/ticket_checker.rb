bc30692cc20c223e9348fd7c325917e82be19f9f
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-01-07 14:07:42 +0900

    fix initialize

--- lib/kippu/ticket_checker.rb
+++ lib/kippu/ticket_checker.rb
@@ -1,5 +1,11 @@
 module Kippu
   class TicketChecker
+    attr_reader :station
+
+    def initialize(args={})
+      @station = args[:station]
+    end
+
     def admit(ticket)
       true
     end
--- spec/ticket_checker_spec.rb
+++ spec/ticket_checker_spec.rb
@@ -1,18 +1,33 @@
 require 'spec_helper'
 
 describe Kippu::TicketChecker do
-  let(:ticket_checker) { Kippu::TicketChecker.new }
+  let(:ticket_checker) { Kippu::TicketChecker.new(station: station) }
   let(:ticket) { spy('ticket') }
 
   describe 'admit' do
+    let(:station) { 'umeda' }
+
     example do 
       expect(ticket_checker.admit(ticket)).to be true
     end
   end
 
   describe 'out' do
+    let(:station) { 'jyuso' }
+
     example do 
       expect(ticket_checker.out(ticket)).to be true
+    end
+  end
+
+  describe 'scenario 1' do
+    example do 
+      ticket = Kippu::Ticket.new(fare: 150)
+      umeda_ticket_checker = Kippu::TicketChecker.new(station: 'umeda')
+      jyuso_ticket_checker = Kippu::TicketChecker.new(station: 'jyuso')
+
+      expect(umeda_ticket_checker.admit(ticket)).to be true
+      expect(jyuso_ticket_checker.out(ticket)).to be true
     end
   end
 end
